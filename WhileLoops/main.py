008c480a5128e0a6382a81017056ab7e2667ae01
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-04-13 08:35:11 -0400

    update

--- WhileLoops/main.py
+++ WhileLoops/main.py
@@ -8,8 +8,14 @@ while i >= 0:
 """
 
 
-for i in range(100, -1, -1):
+"""for i in range(100, -1, -1):
     if i == 0:
+        print("Boom!") 
+    else:
+        print(i)"""
+
+for i in range(1, 101):
+    if i == 100:
         print("Boom!") 
     else:
         print(i)
