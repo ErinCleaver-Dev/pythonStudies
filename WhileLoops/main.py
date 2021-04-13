a89db8df41bc61cbbee555d633e1b4d63121d699
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-04-13 08:32:02 -0400

    Update main.py
    update

--- WhileLoops/main.py
+++ WhileLoops/main.py
@@ -1,7 +1,17 @@
-i = 100
+"""i = 100
 while i >= 0:
     if i == 0:
         print("Boom!") 
     else:
         print(i)
     i-=1
+"""
+
+
+for i in range(100, -1, -1):
+    if i == 0:
+        print("Boom!") 
+    else:
+        print(i)
+
+input()
