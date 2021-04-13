b5d084e9508ddd20b4ce0bd187e2ba7604f04b1a
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-04-13 06:08:59 -0400

    Updated

--- Turtle/main.py
+++ Turtle/main.py
@@ -12,9 +12,22 @@ def myTurtle(forward, rotateLeft):
     my_turtle.forward(forward)
 
 myTurtle(90, 90)
-
-HelloWorld = "Hello World"
-
-print(HelloWorld)
+my_turtle.forward(90)
+myTurtle(90, 90)
+myTurtle(90, 90)
+myTurtle(90, 90)
+myTurtle(90, 90)
+my_turtle.forward(90)
+my_turtle.left(90)
+my_turtle.forward(90)
+myTurtle(90, 90)
+my_turtle.right(180)
+my_turtle.forward(90)
+my_turtle.right(90)
+myTurtle(90, 90)
+my_turtle.right(180)
+my_turtle.forward(90)
+my_turtle.right(90)
+myTurtle(90, 90)
 
 input()
